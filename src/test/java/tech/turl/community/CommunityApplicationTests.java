eb1fe029c56fce8b49736f5efea077a63e40f62c
zhengguohuang <838667533@qq.com>
2021-03-08 13:58:52 +0800

    创建项目

---
+++ src/test/java/tech/turl/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.turl.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
