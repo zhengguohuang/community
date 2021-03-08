f29ec29896f7002db9f93423ce2fa8aebabfca32
zhengguohuang <838667533@qq.com>
2021-03-08 14:30:09 +0800

    添加测试controller

--- src/main/java/tech/turl/community/controller/AlphaController.java
+++ src/main/java/tech/turl/community/controller/AlphaController.java
@@ -1,4 +1,9 @@
 package tech.turl.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class AlphaController {
 }
