69984cc07d97a597b5fc85ce2d77dff5d2cda3f0
zhengguohuang <838667533@qq.com>
2021-03-08 14:45:50 +0800

    修改测试controller 配置访问端口，项目路径

--- src/main/java/tech/turl/community/controller/AlphaController.java
+++ src/main/java/tech/turl/community/controller/AlphaController.java
@@ -2,8 +2,14 @@ package tech.turl.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping
+@RequestMapping("/alpha")
 public class AlphaController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello Spring Boot.";
+    }
 }
