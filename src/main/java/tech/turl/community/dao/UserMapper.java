2ac26da0c057aa5f142bd8545bce729a76b4311c
zhengguohuang <838667533@qq.com>
2021-03-09 21:27:23 +0800

    整合Mybatis

---
+++ src/main/java/tech/turl/community/dao/UserMapper.java
@@ -0,0 +1,23 @@
+package tech.turl.community.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import tech.turl.community.entity.User;
+
+@Mapper
+public interface UserMapper {
+    User selectById(int id);
+
+    User selectByName(String username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+    int updateStatus(@Param("id") int id, @Param("status") int status);
+
+    int updateHeader(@Param("id") int id, @Param("headerUrl") String headerUrl);
+
+    int updatePassword(@Param("id") int id, @Param("password") String password);
+
+}
---
+++ src/test/java/tech/turl/community/TestMapper.java
@@ -0,0 +1,58 @@
+package tech.turl.community;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import tech.turl.community.dao.UserMapper;
+import tech.turl.community.entity.User;
+
+import java.util.Date;
+
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TestMapper {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.abc.com/1.png");
+        user.setCreateTime(new Date());
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+    @Test
+    public void updateUser(){
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+
+
+
+    }
+
+}
